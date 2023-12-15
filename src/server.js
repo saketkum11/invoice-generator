2b05ba21b5bce3974db461e6769c28c08e80418c
Saket kumar <kumarsaket601@gmail.com>
2023-12-16 00:22:10 +0530

    import  and export file

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import { Home } from "./pages/Home";
+import { Invoice } from "./pages/Invoice";
+
+// component
+import { Header } from "./components/Header";
+import { InvoiceList } from "./components/InvoiceList";
+import { Button } from "./components/Button";
+import { ListItem } from "./components/ListItem";
+// export
+export { Home, Button, ListItem, Invoice, InvoiceList, Header };
